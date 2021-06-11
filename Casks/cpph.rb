c0b696adb69a75b04000d98e736c2c075d9316fd
doerfli <doerfli@users.noreply.github.com>
2021-06-11 17:26:10 +0200

    Update cpph.rb

--- Casks/cpph.rb
+++ Casks/cpph.rb
@@ -1,5 +1,5 @@
 cask "cpph" do
-  version "1.0.0"
+  version "1.0.1"
   sha256 "379997ec7fc998f282316c18abd3966369cc4f0c50a47268dd3b98ef99ee76b1"
 
   url "artifact.devres.internal.adcubum.com/artifactory/adcubum-library-releases/com/adcubum/clientpush/adcubum-clientpush-installer/mac/CPProtocolHandler.pkg"
