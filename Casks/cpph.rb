3d1e145a409ff944f176325752011cc1b755c4a6
Patrice Manser <patrice.manser@adcubum.com>
2022-05-24 08:53:49 +0200

    Updated link to use https instead of http

--- Casks/cpph.rb
+++ Casks/cpph.rb
@@ -2,7 +2,7 @@ cask "cpph" do
   version "1.4.2"
   sha256 "6fde55ae6a63a411a51772d4908379292c65dd27d7fd886f714a9e6bb7f7cca5"
 
-  url "artifact.devres.internal.adcubum.com/artifactory/adcubum-library-releases/com/adcubum/clientpush/adcubum-clientpush-installer/mac/CPProtocolHandler-1.4.2.pkg"
+  url "https://artifact.adcubum.com/artifactory/adcubum-library-releases/com/adcubum/clientpush/adcubum-clientpush-installer/mac/CPProtocolHandler-1.4.2.pkg"
   name "Clientpush Protocolhandler"
   desc "used to start adcubum SYRIUS (ERP Core/FX Client) from a third party application"
 
