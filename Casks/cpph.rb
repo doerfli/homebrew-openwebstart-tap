4ec9dac5189a0a6629f915f49c76cfae3c455679
zruzak <zeljko.ruzak@gmail.com>
2022-06-09 08:46:56 +0200

    Update cpph.rb

--- Casks/cpph.rb
+++ Casks/cpph.rb
@@ -1,12 +1,12 @@
 cask "cpph" do
-  version "1.4.2"
-  sha256 "6fde55ae6a63a411a51772d4908379292c65dd27d7fd886f714a9e6bb7f7cca5"
+  version "1.4.3"
+  sha256 "22c0def063afd0ee3aa6566fb1957f3609aae3971ad8d4c5d3cbfefeba2fa86e"
 
-  url "https://artifact.adcubum.com/artifactory/adcubum-library-releases/com/adcubum/clientpush/adcubum-clientpush-installer/mac/CPProtocolHandler-1.4.2.pkg"
+  url "https://artifact.adcubum.com/artifactory/adcubum-library-releases/com/adcubum/clientpush/adcubum-clientpush-installer/mac/CPProtocolHandler-1.4.3.pkg"
   name "Clientpush Protocolhandler"
   desc "used to start adcubum SYRIUS (ERP Core/FX Client) from a third party application"
 
-  pkg "CPProtocolHandler-1.4.2.pkg"
+  pkg "CPProtocolHandler-1.4.3.pkg"
 
   uninstall pkgutil: [
     "com.adcubum.pkg.CPProtocolHandler"
