fde53797144ea65993692a9e41ce9774dc5b6f1b
zruzak <zeljko.ruzak@gmail.com>
2022-06-15 10:18:46 +0200

    1.4.3 - adjust sha256

--- Casks/cpph.rb
+++ Casks/cpph.rb
@@ -1,6 +1,6 @@
 cask "cpph" do
   version "1.4.3"
-  sha256 "22c0def063afd0ee3aa6566fb1957f3609aae3971ad8d4c5d3cbfefeba2fa86e"
+  sha256 "2351a0db4d6a5a22a6aa84867055826f14bbbea27dd9f2a6a8db3725a03b1b25"
 
   url "https://artifact.adcubum.com/artifactory/adcubum-library-releases/com/adcubum/clientpush/adcubum-clientpush-installer/mac/CPProtocolHandler-1.4.3.pkg"
   name "Clientpush Protocolhandler"
